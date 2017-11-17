eba6816538c210df08ddb0b670305494ddfdeeda
Pentabyteman <nils@brennbach.de>
2017-11-17 23:06:07 +0100

    Update manage.py

--- web_dev/sas_web/manage.py
+++ web_dev/sas_web/manage.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 import os
 import sys
+import import platform
+
 
 if __name__ == "__main__":
+    operating_system = platform.platform
+    if operating_system.startswith( "Darwin" ):
+        import pymysql
+        pymysql.install_as_MySQLdb()
+    
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sas_web.settings")
 
     from django.core.management import execute_from_command_line
