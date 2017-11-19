14f44f7819714a4d0931e409eecd96aa17949d14
erichhasl <erich.hasl@gmail.com>
2017-11-19 15:49:31 +0100

    fix 404

--- web_dev/sas_web/startpage/urls.py
+++ web_dev/sas_web/startpage/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^impressum', views.impressum, name='impressum'),
-    url(r'^', views.index, name='index')
+    url(r'^$', views.index, name='index')
 ]
