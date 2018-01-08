185523587dd97bd9cebad1b15bf65562f63ebbc1
erichhasl <erich.hasl@gmail.com>
2018-01-08 22:11:50 +0100

    deactivate party registration

--- web_dev/sas_web/meingoethopia/urls.py
+++ web_dev/sas_web/meingoethopia/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^betrieb/anmelden', views.betrieb_new, name='betrieb_new'),
-    url(r'^partei/gruenden', views.partei_new, name='partei_new'),
-    url(r'^praesident/kandidieren', views.praesident_werden,
-        name='praesident_werden'),
+    # url(r'^partei/gruenden', views.partei_new, name='partei_new'),
+    # url(r'^praesident/kandidieren', views.praesident_werden,
+    #     name='praesident_werden'),
     url(r'^frage/stellen', views.question_new, name='question_new')
 ]
