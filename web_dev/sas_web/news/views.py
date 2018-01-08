183565f8568d35315f627bbb17164bd371c4a712
erichhasl <erich.hasl@gmail.com>
2018-01-08 13:06:06 +0100

    add partei werbung

--- web_dev/sas_web/news/views.py
+++ web_dev/sas_web/news/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+import math
+
+from .models import ParteiWerbung
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'news/index.html')
+    objects = ParteiWerbung.objects.all()
+    rows = group(objects, 3)
+    return render(request, "news/index.html", {'rows': rows,
+                                               })
+
+
+def group(l, n):
+    return [l[k*n:k*n+n] for k in range(math.ceil(len(l) / n))]
