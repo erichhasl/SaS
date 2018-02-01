cef2e4914892ac3f3b0abc3abd4fb69bf4effe8f
erichhasl <erich.hasl@gmail.com>
2018-02-01 17:51:34 +0100

    nuke advertisement

--- web_dev/sas_web/news/urls.py
+++ web_dev/sas_web/news/urls.py
@@ -3,7 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^partei/(?P<partei_id>[0-9]+)', views.partei, name='partei'),
-    url(r'^praesident/(?P<praesident_id>[0-9]+)', views.praesident, name='praesident')
+    url(r'^$', views.index, name='index')
 ]
--- web_dev/sas_web/news/views.py
+++ web_dev/sas_web/news/views.py
@@ -1,29 +1,6 @@
 from django.shortcuts import render
-import math
-
-from .models import ParteiWerbung, PraesidentWerbung
 
 
 # Create your views here.
 def index(request):
-    partei_objs = ParteiWerbung.objects.all()
-    parteien = group(partei_objs, 3)
-
-    praesident_objs = PraesidentWerbung.objects.all()
-    praesidenten = group(praesident_objs, 3)
-    return render(request, "news/index.html",
-                  {'parteien': parteien, 'praesidenten': praesidenten})
-
-
-def partei(request, partei_id):
-    return render(request, "news/partei.html",
-                  {'entry': ParteiWerbung.objects.get(pk=partei_id)})
-
-
-def praesident(request, praesident_id):
-    return render(request, "news/praesident.html",
-                  {'entry': PraesidentWerbung.objects.get(pk=praesident_id)})
-
-
-def group(l, n):
-    return [l[k*n:k*n+n] for k in range(math.ceil(len(l) / n))]
+    return render(request, "news/index.html")
